1096a2c9c3530c551f108a28f28bf2b475ad1bab
Nils Löwe <nils.loewe@wlw.de>
2016-04-06 09:02:11 +0200

    puma config for capistrano deployment

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,9 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+#!/usr/bin/env puma
 
-preload_app!
+threads 0, 4
+workers 1
 
-rackup DefaultRackup
-port ENV['PORT'] || 3000
-environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+bind  "unix:///var/tmp/project.sock"
+pidfile "/var/run/puma/project.pid"
+environment "production"
+stdout_redirect "/var/log/puma/project.log"
